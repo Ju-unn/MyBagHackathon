af897c39eb330a9002d49fc2c26bb91f8ac69ae7
junseok <s4538@naver.com>
2026-08-03 16:56:50 +0900

    test : git 테스트 메시지 입니다

--- app/src/main/java/com/example/mybaghackathon/MainActivity.java
+++ app/src/main/java/com/example/mybaghackathon/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+            // 깃 테스트 메시지 입니다
         });
     }
 }
