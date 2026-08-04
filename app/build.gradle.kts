448e8b75cc6dcd573e602194c7a879089571a021
shin1231231 <2018rk@naver.com>
2026-08-04 16:40:35 +0900

    feat: UI 및 컴포넌트 1차 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.mybaghackathon"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mybaghackathon"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
