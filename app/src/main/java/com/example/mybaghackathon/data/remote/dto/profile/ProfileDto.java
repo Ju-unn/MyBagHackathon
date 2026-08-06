3d091d3e65e4d6e488960653143ff589c819dc7c
junseok <s4538@naver.com>
2026-08-07 06:24:53 +0900

    refactor: 빈 클래스 삭제 및 README.md 최신화

--- app/src/main/java/com/example/mybaghackathon/data/remote/dto/profile/ProfileDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.mybaghackathon.data.remote.dto.profile;
-
-// User profile request and response DTO.
-public class ProfileDto {
-}
--- app/src/main/java/com/example/mybaghackathon/data/repository/ProfileRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.mybaghackathon.data.repository;
-
-// 사용자 프로필과 기본 물품 데이터 접근 규칙
-public interface ProfileRepository {
-}
