25481a92a71cd07023a404250b0a5b716969e6fb
junseok <s4538@naver.com>
2026-08-11 11:33:14 +0900

    chore: applicationId를 com.mybagteam.mybag로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.mybaghackathon"
+        applicationId = "com.mybagteam.mybag"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
